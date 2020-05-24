b15935c957b6bef24b522212c5ef6e738877e014
marcoavfcc01121979 <marcoavfcc@gmail.com>
2020-05-24 18:04:55 -0300

    second commit

--- src/components/Picker/index.android..js
+++ src/components/Picker/index.android..js
@@ -7,8 +7,8 @@ const Picker = ({onChange}) => {
     <PickerView>
       <RNPickerSelect
         style={{
-          inputAndroid: {
-            //inputIOS: {
+          //inputAndroid: {
+          inputIOS: {
             height: 50,
             padding: 5,
             backgroundColor: '#FFF',
